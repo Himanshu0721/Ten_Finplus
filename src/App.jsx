e47ac538ccc1e0c09f1b5744bdaf0eb909d90e29
Utkarsh Gautam <113176910+UtkarshGautam12@users.noreply.github.com>
2024-12-10 20:24:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/home/Home'
-import Contact from './components/pages/contact/Contact'
+import Contact from './components/pages/contact/ContactForm'
 import About from './components/pages/about/About'
 
 function App() {
